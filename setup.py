5eb9ee6e7fdd1dcecaad6c7e5cc04e0438bf0065
ErikSchreiner <58606584+ErikSchreiner@users.noreply.github.com>
2024-01-26 17:19:58 -0500

    Changed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='meer_data_analysis',
+    name='meerDataAnaylsis',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
